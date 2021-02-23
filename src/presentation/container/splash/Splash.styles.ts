e0ebc66e05dc9cb9d2379283fdbd7dd02b4c5b0f
Vũ Trường Giang <giang.vu@alta.com.vn>
2021-02-23 18:00:46 +0700

    update

---
+++ src/presentation/container/splash/Splash.styles.ts
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+export const styles = StyleSheet.create({
+  container: {flex: 1},
+});
